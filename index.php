5f9e4798237410b010f21508e38b5d6b70955b12
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-14 15:55:47 +0600

    add some code

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
-    echo "hello world";
-    echo "hello all";
 
+
+    /*echo "hello world";
+    echo "hello all";*/
+    
+
+    // class abc{
+    //    public $name = 'jhon'; // global
+
+    //    static $phone = 49875454;
+
+    //    public function myinfo(){
+    //        $age = 45; //local
+    //    }
+    // }
+
+    
+
+    $name = ['rahim','karim'];
+    
+    echo "<pre>";
+    print_r($name); //array data
+    
+    echo "</pre>";
+
+    $str = implode(',',$name);
+
+    echo $str.' jegfdjf';
 
 
 ?>
