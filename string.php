3d11620fd0dcec25bcd359b1bbdc3f7958de9b21
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-15 15:56:03 +0600

    learn about string method

---
+++ string.php
@@ -0,0 +1,62 @@
+<?php
+    // $str = "hello world gtdry";
+
+    // echo strlen($str); // length
+
+    // echo strrev($str); // reverse
+
+    // echo str_word_count($str); // word count
+
+    // echo strpos($str,'world'); // search
+
+    // echo str_replace('hello','bye',$str); // replace
+
+    $string = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi eaque ea doloribus voluptatem asperiores suscipit dolore. Velit officia excepturi vero, doloremque ex omnis deserunt ad reiciendis quae ratione. Commodi, soluta.";
+
+    // echo $string;
+
+    // $strcut = substr($string,0,30);
+
+    // echo $strcut.'<a href="#">Read More</a>';
+
+    // $valid =  trim($string);
+
+    // if($valid != ''){
+        
+    // }
+
+    // echo strtolower($string);
+    // echo strtoupper($string);
+
+    // $num  = 4525;
+
+    // echo ucfirst($string);
+
+    //echo ucwords($string);
+
+    $name = 'gdf';
+
+    //existing variable check
+    // if(isset($name)){
+    //     echo 'set';
+    // }else{
+    //     echo 'unset';
+    // }
+
+    //existing value fill or not
+
+    // if(empty($name)){
+    //     echo 'unset';
+    // }else{
+    //     echo 'set';
+    // }
+
+    // echo $name;
+
+
+    
+
+
+
+?>
+
