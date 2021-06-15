b3bde4e4354cd6f283a1f18b4888371b0505888e
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-15 15:14:41 +0600

    learn basic datatype

---
+++ datatype.php
@@ -0,0 +1,40 @@
+
+
+<?php
+//string
+//integer
+//float
+//boolean
+//object //oop concept
+//array // array class
+//NULL
+
+// $name = 'hello "w"orld'; 
+// // echo $name;
+// // $num = 4512; // int
+// // $num = 4.2535;
+
+// // var_dump($name).'<br>';
+// $arr = [1,'2'];
+
+// echo '<pre>';
+// var_dump($arr); 
+// echo '</pre>';
+
+//  $a = '';
+// $b = 10;
+
+// if($a == NULL){
+//     echo "Faka";
+// }else{
+//     echo "fill";
+// }
+
+
+
+
+
+
+
+
+?>
