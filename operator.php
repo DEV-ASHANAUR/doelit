25b1d2ec607ecc604f809c5abeb3dc495f90e1b9
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-16 22:00:05 +0600

    learn about operator

---
+++ operator.php
@@ -0,0 +1,63 @@
+<?php
+    // Arithmetic operators
+
+    $x = 10;
+    $y = 4;
+
+    echo 'sum = '.($x + $y).'<br>';
+    echo 'sub = '.($x - $y).'<br>';
+    echo 'mul = '.($x * $y).'<br>';
+    echo 'div = '.($x / $y).'<br>';
+    echo 'mod = '.($x % $y).'<br>';
+    echo 'power = '.($x ** $y).'<br>';
+
+    //Assignment Operators
+
+    // $x = 67; 
+
+    // $z = $x+$y;
+
+    // echo $x += $y; 
+
+    // $sum = 0;
+
+    // for ($i=0; $i <= 10; $i++) { 
+    //     $sum += $i;
+    // }
+
+    // echo $sum;
+
+    // echo $z;
+
+    // $x = 10;
+    // $y = 4;
+    // //type check    
+    // if($x === $y){
+    //     echo 'match';
+    // }else{
+    //     echo 'dont match';
+    // }
+
+    //type check    
+    // if($x != $y){
+    //     echo 'not';
+    // }else{
+    //     echo 'yes';
+    // }
+
+    $x = true;
+
+    if(!$x){
+        echo 'not';
+    }else{
+        echo 'yes';
+    }
+
+
+
+
+
+
+
+
+?>
