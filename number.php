f30c7a15ef4d5302278bd38b68558eb5e9990fab
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-16 15:43:54 +0600

    learn about number method

---
+++ number.php
@@ -0,0 +1,87 @@
+<?php
+    // $num = 45226;
+
+    // var_dump(is_int($num)); // check int // return true or false
+ 
+    //var_dump(is_float($num)); // check float // return true or false
+
+    // $x = 1.9e411;
+
+    // $x = pi(); // pi er value
+
+    // echo $x;
+
+    // $x = acos(8); 
+
+    // echo $x;
+
+    // var_dump($x);
+
+    // $y = "48dgdggfdg75445";
+
+    // $x = "1dfhg00";
+    // $y = 10;
+
+
+    // // echo $z;
+
+    // if(is_numeric($x) && is_numeric($y)){
+    //     $z = $x + $y;
+    //     echo $z;
+    // }else{
+    //     echo 'please provide numaric number';
+    // }
+
+    // var_dump(is_numeric($y));
+
+    // convert integer to float
+
+    // $num = 4125.25;
+
+    // $int = (int)$num;
+
+    // echo $int;
+
+    // $num = "45125";
+
+    // echo $num;
+
+    // $int = (int)$num;
+
+    // echo $int;
+
+
+    // $x = -451.78;
+
+    // $y = abs($x); //
+
+    // echo $y;
+
+    // $x = 4.4;
+
+    // echo round($x); // auto 
+
+    // $math  = 66.40;
+
+    // echo floor($math); // down
+
+    // echo ceil($math); // upper
+
+    // echo min(45,25,48,58,58,-58); // min
+    // echo max(45,25,48,58,58,-58); // max
+
+    // echo sqrt(64); // root
+
+    // echo rand(10,100); 
+
+
+
+
+
+
+
+
+
+
+
+?>
