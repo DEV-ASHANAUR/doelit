63000d8b0325fa885ab4c2892c0164129ed40785
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-17 15:11:36 +0600

    learn about condition

---
+++ condition.php
@@ -0,0 +1,68 @@
+<?php
+    // if condition
+
+    $x = 10;
+
+    // if($x > 5){
+    //     echo $x.' is greater than 5';
+    // }else{
+    //     echo $x.' is less than 5';
+    // }
+
+    //  echo $y =  ($x < 5) ? 'big': 'small'; //Ternary
+
+    //   $user = 'jhon';
+
+    //  echo $test = $user ?? 'rakib'; //	Null coalescing
+
+        // if(isset($user)){
+        //     echo $user;
+        // }else{
+        //     echo 'rakib';
+        // }
+
+    // if else if
+
+    // $mark = 100;
+
+    // if($mark >= 33 && $mark <= 39){
+    //     echo 'D grade';
+    // }else if($mark >= 40 && $mark <= 49){
+    //     echo 'C grade';
+    // }else if($mark >= 50 && $mark <= 59){
+    //     echo 'B grade';
+    // }else if($mark >= 60 && $mark <= 69){
+    //     echo 'A- grade';
+    // }else if($mark >= 70 && $mark <= 79){
+    //     echo 'A grade';
+    // }else if($mark >= 80 && $mark <= 100){
+    //     echo 'A+ grade';
+    // }else{
+    //     echo "You Are Fail";
+    // }
+
+    // $x = 'cdsgfd';
+
+    // switch ($x) {
+    //     case 'green':
+    //         echo 'My fav color is Green';
+    //         break;
+    //     case 'yellow':
+    //         echo 'My Fav Color Is Yellow';
+    //         break;
+    //     case 'red':
+    //         echo 'My Fav Color Is Red';
+    //         break;
+    //     default:
+    //         echo "No Match";
+    //         break;
+    // }
+
+    
+
+
+
+
+
+
+?>
