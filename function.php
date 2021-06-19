7b1443350e863190e1fae6f3c118604869acb746
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-19 12:31:04 +0600

    learn about function

---
+++ function.php
@@ -0,0 +1,47 @@
+<?php
+
+    // about function 
+
+//     function Myfun(){
+//        $abc = 'hello world <br>';
+//        $abd = 'hello';
+//        return $abc;
+//        echo $abd;
+       
+//     }
+
+//    echo Myfun();
+
+//     // how to give arg
+
+//     function sum(int $a,int $b){
+//         $c = $a + $b;
+//         return $c;
+//     }
+
+//     function sub($a,$b){
+//         $c = $a - $b;
+//         return $c;
+//     }
+    
+
+//    echo sum(45,5).'<br>';
+
+//    echo sub(40,5);
+        
+//     function sum(int $a,int $b){
+//         $c = $a + $b;
+//         return $c;
+//     }
+
+//    echo sum(4,6);
+
+   
+
+
+
+
+    
+
+
+?>
