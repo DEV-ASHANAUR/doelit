eb7c7632eda875fb21093f76dbbaa9af330f6b8d
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-20 12:51:40 +0600

    learn about index array

---
+++ array.php
@@ -0,0 +1,81 @@
+<!-- index array -->
+
+<?php
+    // $arr1 = array(); // 
+    // $arr = []; //
+
+    // $arr = [];
+    // $arr[1] = 45; // insert data 
+
+    // array_push($arr,34,78,343); // insert data last
+
+    // array_pop($arr); // delete from last
+
+    // array_shift($arr); // delete from first
+
+    // array_unshift($arr,78); // insert from first
+
+    // $newArr = ['html','css','javascript','php','java'];
+
+    // echo '<pre>';
+    //     print_r($newArr);
+    // echo '<pre>';
+
+    // $x = array_splice($newArr,2);
+
+    // echo '<pre>';
+    //     print_r($x);
+    // echo '<pre>';
+
+    // $arr1 = [78,'dfd'];
+
+    // $arr2 = [78,'fdf'];
+
+    // print_r(array_merge($arr1,$arr2));
+
+    $arr = [1,2,3,4,5,6];
+
+    // echo $arr[0]; 
+
+    // $arr[0] = 45;
+    // $arr[1] = 50;
+    // // sort($arr); // assending
+    // // rsort($arr); // decending
+    // // print_r($arr);
+    // echo '<pre>';
+    //     print_r($arr);
+    // echo '<pre>';
+
+    // echo count($arr); // array length
+
+    // $sum = 0;
+
+    // for($i = 0; $i < count($arr); $i++){
+    //     $sum = $sum + $arr[$i];
+    // }
+
+    // echo $sum;
+    // value print by foreach
+    // foreach($arr as $key => $val){
+    //     echo $key .' : '. $val.'<br>';
+    // }
+
+    // sum by foreach
+    $sum = 0;
+    foreach($arr as $val){
+        $sum += $val;
+    }
+
+    echo $sum;
+
+    // value print by for loop
+
+    // for($i = 0; $i < count($arr); $i++){
+    //     echo $arr[$i].'<br>';
+    // }
+
+
+
+
+
+?>    
