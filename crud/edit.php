a5e421d3b8566b1e5e7088695c65ede778e2904e
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-23 13:23:32 +0600

    title change

--- crud/edit.php
+++ crud/edit.php
@@ -32,7 +32,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home page | Simple crud</title>
+    <title>Edit page | Simple crud</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
