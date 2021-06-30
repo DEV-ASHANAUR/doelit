0e46cabeb9c4d6a6034b9682d4f7ec01e186d0fa
Md. Ashanaur Rahman <devashanaur009@gmail.com>
2021-06-29 17:52:45 -0700

    test git

--- crud/show.php
+++ crud/show.php
@@ -37,7 +37,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Show page | Simple crud</title>
+    <title>All Data page | Simple crud</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
