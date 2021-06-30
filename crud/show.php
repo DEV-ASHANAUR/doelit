3574a4419f64a45c85d59d0c2d17e79d3afde12c
Md. Ashanaur Rahman <devashanaur009@gmail.com>
2021-06-29 17:58:09 -0700

    test

--- crud/show.php
+++ crud/show.php
@@ -23,13 +23,7 @@
             $user_email = $data->email;
         }
     }
-
-
-  
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +31,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>All Data page | Simple crud</title>
+    <title>All Data show page | Simple crud</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
